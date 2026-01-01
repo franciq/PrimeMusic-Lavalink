7fce73d1617cdb5ab5d2c6f8d2c83985d73fdc42
Franciq <franeekk.5@gmail.com>
2026-01-01 17:58:38 +0100

    Change bot TOKEN in config.js
    Updated the bot TOKEN for security reasons.

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 
 
 module.exports = {
-  TOKEN: "MTQ1NjI4Mjk5OTYzNjA5OTE3Nw.GwSUiI.CNgHA-JfurtjI1-rMSSolsR_An_SXPb7gQ1eOQ"
+  TOKEN: "MTQ1NjI4Mjk5OTYzNjA5OTE3Nw.GS71vb.ep2K_j5nX1pygzprCaMUIKsPCM6S34k97xcfYs"
   language: "en",
   ownerID: ["1444334428003504223", ""], 
   mongodbUri : "mongodb+srv://shiva:shiva@discordbot.opd5w.mongodb.net/?retryWrites=true&w=majority",
